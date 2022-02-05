4e285a0460c654ff4288cfba709a4e98e75d144f
swang <swang@c1r4s3.42seoul.kr>
2022-02-05 16:36:50 +0900

    테스트문 수정

--- cpp01/ex06/main.cpp
+++ cpp01/ex06/main.cpp
@@ -6,7 +6,7 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/04 16:56:38 by swang             #+#    #+#             */
-/*   Updated: 2022/02/04 19:55:12 by swang            ###   ########.fr       */
+/*   Updated: 2022/02/05 16:34:52 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,14 @@ int	main(int argc, char **argv)
 	if (argc == 1)
 	{
 		std::cout << "Need log level\n";
-		return (0);
+		return (-1);
 	}
 	else if (argc > 2)
 	{
 		std::cout << "too many argument\n";
-		return (0);
+		return (-1);
 	}
 	Karen karen;
-
 	karen.filter(argv[1]);
 	return (0);
 }
