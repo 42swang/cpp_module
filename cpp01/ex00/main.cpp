fdcd4bdabdec37e8ce600c9a47563dc9858589c3
swang <swang@MacBook-sh.local>
2022-02-06 09:08:18 +0900

    new 좀비도 자기소개 하도록 수정

--- cpp01/ex00/main.cpp
+++ cpp01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/03 11:24:40 by swang             #+#    #+#             */
-/*   Updated: 2022/02/03 12:26:36 by swang            ###   ########.fr       */
+/*   Updated: 2022/02/06 08:56:31 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 int	main()
 {
 	Zombie* new_zombie = newZombie("newbie");
+	new_zombie->announce();
 	delete new_zombie;
 	randomChump("random");
 	return (0);
