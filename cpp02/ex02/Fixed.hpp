52d740cc7abd172778a182cc6ab6fe0fadf101be
swang <swang@MacBook-sh.local>
2022-02-08 23:06:19 +0900

    Add comments Fixed::min(), Fixed::max() && check pdf

--- cpp02/ex02/Fixed.hpp
+++ cpp02/ex02/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 15:47:27 by swang             #+#    #+#             */
-/*   Updated: 2022/02/08 21:36:52 by swang            ###   ########.fr       */
+/*   Updated: 2022/02/08 23:02:39 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -45,14 +45,12 @@ public:
 	Fixed& 	operator--();
 	Fixed 	operator++(int);
 	Fixed 	operator--(int);
+//	객체를 생성하지 않고 클래스 이름만으로 함수를 호출하는 Fixed::min 의 형식으로 사용하려면 static으로 선언해야함.
 	static Fixed&	min(Fixed& fix1, Fixed& fix2);
-	static const Fixed&	min(const Fixed& fix1, const Fixed& fix2);
-//두 개의 고정 소수점 값 참조를 받아 가장 작은 값의 참조를 반환하는 [정적] 멤버 함수 min과
-//두 개의 고정 소수점 (상수) 값의 참조를 받아 가장 작은 (상수) 값의 참조를 반환하는 오버로딩
 	static Fixed&	max(Fixed& fix1, Fixed& fix2);
+	static const Fixed&	min(const Fixed& fix1, const Fixed& fix2);
 	static const Fixed&	max(const Fixed& fix1, const Fixed& fix2);
-//두 개의 고정 소수점 값 참조를 받아 가장 큰 값의 참조를 반환하는 정적 멤버 함수 max와,
-//두 개의 고정 소수점 상수 값의 참조를 받아 가장 큰 상수 값의 참조를 반환하는 오버로딩
+//	ㄴ> static을 떼면 멤버함수처럼 a.min(a,b)로 사용할 수 있음
 	~Fixed();
 };
 
--- cpp02/ex02/main.cpp
+++ cpp02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/07 15:45:59 by swang             #+#    #+#             */
-/*   Updated: 2022/02/08 21:43:50 by swang            ###   ########.fr       */
+/*   Updated: 2022/02/08 23:02:37 by swang            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ int main( void )
 	Fixed a;
 	Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
 
+
 	std::cout << a << std::endl;
 	std::cout << ++a << std::endl;
 	std::cout << a << std::endl;
@@ -26,9 +27,12 @@ int main( void )
 
 	std::cout << b << std::endl;
 	std::cout << std::endl;
-	
+
+//	b가 const 이므로 매개변수가 const인 max, min함수를 호출함
 	std::cout << Fixed::max(a, b) << std::endl;
 	std::cout << Fixed::min(a, b) << std::endl; //0.0078125
+//	const가 아닌 Fixed c;를 인자로 넣으면 매개변수가 const가 아닌 max, min함수를 호출함 
+//	std::cout << Fixed::max(a, c) << std::endl;
     std::cout << std::endl;
 
     std::cout << --a << std::endl; //0.00390625
