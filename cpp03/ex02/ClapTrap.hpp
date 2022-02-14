8eb19dfbc82cd4705ddbacf7494794e1256fda24
swang <swang@MacBook-sh.local>
2022-02-14 19:22:32 +0900

    Add ex02

---
+++ cpp03/ex02/ClapTrap.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ClapTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/09 08:44:37 by swang             #+#    #+#             */
+/*   Updated: 2022/02/11 12:25:19 by swang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+# include <string>
+
+# define C_NRML "\033[0m"
+# define C_RED  "\033[31m" 
+# define C_GREN "\033[32m"
+# define C_YLLW "\033[33m" // clap
+# define C_BLUE "\033[34m"
+# define C_PRPL "\033[35m" // scav
+# define C_AQUA "\033[36m" // frag
+
+class ClapTrap
+{
+protected:
+	std::string		_name;
+	unsigned int	_hitpoint;
+	unsigned int	_energypoint;
+	unsigned int	_attackdamage;
+public:
+	ClapTrap(); //default constructor
+	ClapTrap(std::string name);
+	ClapTrap(const ClapTrap& copy); //copy constructor
+	~ClapTrap(); //destructor
+	ClapTrap&	operator=(const ClapTrap& claptrap); // assignation operator
+	
+	void	attack(std::string const & target);
+	void	takeDamage(unsigned int amount);
+	void	beRepaired(unsigned int amount);
+};
+
+#endif
---
+++ cpp03/ex02/FragTrap.cpp
@@ -0,0 +1,93 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.cpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: swang <swang@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/11 11:49:53 by swang             #+#    #+#             */
+/*   Updated: 2022/02/12 15:52:32 by swang            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "FragTrap.hpp"
+
+FragTrap::FragTrap() : ClapTrap()
+{
+	std::cout << C_AQUA << "default constructor called... " << C_NRML;
+	std::cout << "FragTrap " << C_BLUE << "<default> " << C_NRML << "appeard!!\n";
+	_name = "default";
+	_hitpoint = 100;
+	_energypoint = 100;
+	_attackdamage = 30;
+}
+
+FragTrap::FragTrap(std::string name) : ClapTrap(name)
+{
+	std::cout << C_AQUA << "constructor(parameter of name) called... " << C_NRML;
+	std::cout << "FragTrap " << C_BLUE << "<" << name << "> " << C_NRML << "appeard!!\n";
+	_hitpoint = 100;
+	_energypoint = 100;
+	_attackdamage = 30;
+}
+
+FragTrap::~FragTrap()
+{
+	std::cout << C_AQUA << "destructor called... " << C_NRML;
+	std::cout << "FragTrap " << C_BLUE << "<" << _name << ">" << C_NRML << " destroyed\n";
+}
+
+FragTrap::FragTrap(const FragTrap &copy) : ClapTrap(copy)
+{
+	std::cout << C_AQUA << "copy constructor called... " << C_NRML << std::endl;
+	*this = copy;
+}
+
+FragTrap & FragTrap::operator=(const FragTrap& fragtrap)
+{
+	std::cout << C_AQUA <<  "assignation operator called... " << C_NRML << std::endl;
+	if (this == &fragtrap)
+		return (*this);
+	_name = fragtrap._name;
+	_hitpoint = fragtrap._hitpoint;
+	_energypoint = fragtrap._energypoint;
+	_attackdamage = fragtrap._attackdamage;
+	return (*this);
+}
+
+void	FragTrap::highFivesGuys(void)
+{
+	if (this->_hitpoint == 0)
+	{
+		std::cout << C_AQUA << "I can't High-Five.. I need Repair...\n" << C_NRML;
+		return ;
+	}
+	std::cout << C_AQUA << "I Love You Guys! High-Five?!" << C_NRML << std::endl;
+}
+
+void	FragTrap::attack(std::string const & target)
+{
+	std::cout << "<" << this->_name << "> attack " << target << ", causing ";
+	std::cout << this->_attackdamage <<" damage!\n";
+}
+
+void	FragTrap::takeDamage(unsigned int amount)
+{
+	if (this->_hitpoint == 0)
+	{
+		std::cout << "<" << _name << "> need repair. Already 0 HP" << std::endl;
+		return ;
+	}
+	std::cout << "<" << _name << "> take " << amount << " damage... ";
+	if (this->_hitpoint <= amount)
+		this->_hitpoint = 0;
+	else
+		this->_hitpoint -= amount;
+	std::cout << "<" << _name << ">\'s HP is " << _hitpoint << " point\n";
+}
+
+void	FragTrap::beRepaired(unsigned int amount)
+{
+	this->_hitpoint += amount;
+	std::cout << "<" << _name << "> " << amount << " point repaired. <" << _name << ">\'s HP is " << _hitpoint << " point\n";
+}
