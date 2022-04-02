25ee021f7de8fe056fbd22e52a5f20b349c4798a
swang <swang@student.42seoul.kr>
2022-04-02 12:02:58 +0900

    add coment

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -19,8 +19,12 @@ Bureaucrat::Bureaucrat(std::string name, int grade) : _name(name)
 	this->_grade = grade;
 }
 
+//같은 클래스자료형이면 다른 객체여도 같은혈족이라 private멤버변수에 접근이 가능하다
+//프라이빗 멤버변수는 "인스턴스 외부"가 아니라 "클래스 외부"의 접근을 막는다.
+//캡슐화
 Bureaucrat::Bureaucrat(const Bureaucrat& copy) : _name(copy._name)
 {
+	std::cout << "copyName: " << copy._name << std::endl;
 	*this = copy;
 }
 
