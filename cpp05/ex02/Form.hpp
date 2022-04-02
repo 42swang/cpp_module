a490766ac359cfbc8a4503eae1cff56cca89f037
swang <swang@student.42seoul.kr>
2022-04-02 20:01:11 +0900

    서류에 서명 없을 때 예외 추가

--- cpp05/ex02/Form.hpp
+++ cpp05/ex02/Form.hpp
@@ -24,6 +24,13 @@ public:
 	Form(const Form& copy);
 	Form(std::string name, int sign_grade, int execute_grade);
 	Form& operator=(const Form& other);
+	class NotSignedException : public std::exception
+	{
+		const char* what() const throw()
+		{
+			return("Form not signed!");
+		};
+	};
 	class AlreadySignedException : public std::exception
 	{
 		const char* what() const throw()
