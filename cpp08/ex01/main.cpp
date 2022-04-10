c81634554a3dc37a503cee28c9383b63f88be5e9
swang <swang@student.42seoul.kr>
2022-04-11 05:29:57 +0900

    update testcast

--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	std::cout << "simple test\n";
+	std::cout << "----------------simple test--------------------\n";
 	{
 		try
 		{
@@ -12,40 +12,57 @@ int main()
 			sp.addNumber(17);
 			sp.addNumber(9);
 			sp.addNumber(11);
+			std::cout << GREEN;
 			sp.printVec();
+			std::cout << EOC;
 			std::cout << "shortSpan : " <<  sp.shortestSpan() << std::endl;
-			std::cout << "longSpan: " << sp.longestSpan() << std::endl;
+			std::cout << "longSpan : " << sp.longestSpan() << std::endl;
 		}
 		catch (const std::exception &e)
 		{
-			std::cerr << RED << e.what() << EOC << '\n';
+			std::cout << RED << e.what() << EOC << '\n';
 		}
 	}
-	// std::cout << "1 ~ 10000 test\n";
-	// {
-	// 	Span sp(10000);
-	// 	std::vector<int> vector1;
-	// 	for (int i = 0; i < 9900; i++)
-	// 	{
-	// 		if (i < 100)
-	// 			vector1.push_back(i + 1);
-	// 		sp.addNumber(i + 1);
-	// 	}
-	// 	try
-	// 	{
-	// 		sp.addNumber(vector1.begin(), vector1.end());
-	// 		std::cout << "shortestSpan: " << sp.shortestSpan() << std::endl;
-	// 		std::cout << "longestSpan: " << sp.longestSpan() << std::endl;
-	// 		std::cout << "sp size: " << sp.getSize();
-	// 		sp.addNumber(vector1.begin(), vector1.end());
-	// 		std::cout << "shortestSpan: " << sp.shortestSpan() << std::endl;
-	// 		std::cout << "longestSpan: " << sp.longestSpan() << std::endl;
-	// 		std::cout << "sp size: " << sp.getSize();
-	// 	}
-	// 	catch (const std::exception &e)
-	// 	{
-	// 		std::cerr << RED << e.what() << EOC << '\n';
-	// 	}
-	// }
+	std::cout << std::endl;
+	std::cout << std::endl;
+	std::cout << "----------------1 ~ 10000 test--------------\n";
+	{
+		Span sp(10100);
+		std::vector<int> vector1;
+		for (int i = 0; i < 9900; i++)
+		{
+			if (i < 100)
+				vector1.push_back(i + 1);
+			sp.addNumber(i + 1);
+		}
+		try
+		{
+			sp.addNumber(vector1.begin(), vector1.end());
+			std::cout << "shortestSpan: " << sp.shortestSpan() << std::endl;
+			std::cout << "longestSpan: " << sp.longestSpan() << std::endl;
+			std::cout << "sp getSize: " << sp.getSize() << std::endl;
+			std::cout << "sp size: " << sp.getVec().size() << std::endl;
+			std::cout << std::endl;
+			std::cout << std::endl;
+			sp.addNumber(vector1.begin(), vector1.end());
+			std::cout << "shortestSpan: " << sp.shortestSpan() << std::endl;
+			std::cout << "longestSpan: " << sp.longestSpan() << std::endl;
+			std::cout << "sp getSize: " << sp.getSize() << std::endl;
+			std::cout << "sp size: " << sp.getVec().size() << std::endl;
+			std::cout << std::endl;
+			std::cout << std::endl;
+			sp.addNumber(vector1.begin(), vector1.end());
+			std::cout << "shortestSpan: " << sp.shortestSpan() << std::endl;
+			std::cout << "longestSpan: " << sp.longestSpan() << std::endl;
+			std::cout << "sp getSize: " << sp.getSize() << std::endl;
+			std::cout << "sp size: " << sp.getVec().size() << std::endl;
+			std::cout << std::endl;
+			std::cout << std::endl;
+		}
+		catch (const std::exception &e)
+		{
+			std::cout << RED << e.what() << EOC << '\n';
+		}
+	}
 	return 0;
 }
